c503d724e28abdd67863de23f6e79b794a8f230c
Rishabh0059 <130848903+Rishabh0059@users.noreply.github.com>
2025-01-05 06:38:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,13 @@ const app=express();
 const PORT=process.env.port || 5000
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin:[],
+    methods:["POST","GET"],
+    credentials:true
+  }
+));
 
 mongoose
 .connect(process.env.MONGODB_URL)
